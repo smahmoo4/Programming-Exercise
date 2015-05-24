485402008d73264f16038145e85f25777fce5aea
Saad Mahmood <smahmoo4@hawk.iit.edu>
2015-05-24 04:24:24 -0500

    Added CRUD Functionality
    added: create() , read() , update() , delete()
    
    reflected changes in README.txt

--- Entry.java
+++ Entry.java
@@ -1,18 +1,21 @@
-//this class represents an Entry, or a row of information for the CSV file
+	//this class represents an Entry, or a row of information for the CSV file
 
 import java.util.Arrays;
-import java.lang.Comparable; 
-import java.util.Comparator; 
 import java.util.HashMap;
 import java.util.*;
 import java.util.Collections;
 import java.io.*;
 import javax.script.*;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.Color;
+
 
 public class Entry {
-	
+	//global variables
 	static ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-	
+	static String y [] = {""};
+
 	// variables for one row of the CSV file
 	private static String trainLine;
 	private static String routeName;
@@ -65,45 +68,22 @@ public class Entry {
 	public static void setOperatorID(String opID) {
 		operatorID = opID;
 	}
-
-/*------------------------------------------------------*/
-	/* public void sortByRunNumber(Entry [] entries) {
-		Arrays.sort(entries);
-	} */ 
-
-/*------------------------------------------------------*/
-	
-	/* public int compareTo(Entry compareEntry) {
- 
-		String compareRunNumber = compareEntry.getRunNumber(); 
-		
-		return this.runNumber - compareRunNumber;
-	} 
-	
-	public static Comparator<Entry> EntryComparator = new Comparator<Entry>() {
-	 	
-		public int compare(Entry entry1, Entry entry2) {
-	 
-	      String entryName1 = entry1.getRunNumber();
-	      String entryName2 = entry2.getRunNumber();
- 
-	      //ascending order
-	      return entryName1.compareTo(entryName2);
- 
-	      //descending order
-	      //return entryName2.compareTo(entryName1);
-		}
-	}; */
-	
    	
 	/*------------------------------------------------------*/
 	
-	public static void main(String [] args) throws IOException, ScriptException {
+	public static void main(String [] args) throws Exception, IOException {
+		readFromCSVFile();
+		letsCRUD();
+	}
+	
+	/*------------------------------------------------------*/
+	
+	public static void readFromCSVFile() throws IOException, Exception {
 		String fileName = "trains.csv";
 		String line;
 		BufferedReader bufferedReader = null;
 		ArrayList<ArrayList<String>> bigList = new ArrayList<ArrayList<String>>();
-		String y [] = {""};
+		result.clear();
 		try {
 			bufferedReader = new BufferedReader(new FileReader(fileName));
 			
@@ -111,7 +91,6 @@ public class Entry {
 			while ((line = bufferedReader.readLine()) != null) {
 				if(line.contains("TRAIN_LINE")) { //the first entry is the header so handled separately
 					y = line.split(",");
-					System.out.println("\t"+ y[0] + "\t" + y[1] + "\t" + y[2] + "\t" + y[3]);
 				}
 
 				else { //convert each line of the CSV to an ArrayList<String>
@@ -121,71 +100,375 @@ public class Entry {
 					}
 				}
 			}
-			
-			createEntries(bigList);
-			
+						
 			while(bigList.size() > 0) {
 				sortByRunNumber(bigList); 
 			}
 			
 			removeDuplicates(result); //removes duplicate entries
-			displayResult(result); //displays result to console
+			bufferedReader.close();
 		}
 		
 		catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
+
 		
 		finally {
-			try {
-				if (bufferedReader != null) 
-					bufferedReader.close();
-			} 
-			
-			catch (IOException exception) {
-				exception.printStackTrace();
-			}
+				try {
+					if (bufferedReader != null) 
+						bufferedReader.close();
+				} 
+				
+				catch (IOException exception) {
+					exception.printStackTrace();
+				}
 		}
 		
-		//write to file output.csv
-			try {
-				String [] trainData = new String[4];
-				File file = new File("output.txt");
-		 
-				if (file.exists()) {
-					file.createNewFile();
-				}
-	 
-				FileWriter fw = new FileWriter(file.getAbsoluteFile());
-				BufferedWriter bw = new BufferedWriter(fw);
-				bw.write(y[0] + "\t" + y[1] + "\t" + y[2] + "\t" + y[3] + "\n");
+		writeToTextFile();
+	}
+	
+	/*------------------------------------------------------*/
+	
+	public static void writeToTextFile() throws IOException, Exception {
+		try {
+			HashSet <ArrayList<String>> set = new HashSet<ArrayList<String>>();
+			String [] trainData = new String[4];
+			String fileName = "output.txt";
+			File file = new File(fileName);
 				
-				// adding the result list to output.txt
-				for (int i = 0; i < result.size(); i++) {
+			FileWriter fw = new FileWriter(file, false);
+			fw.write(y[0] + "\t" + y[1] + "\t" + y[2] + "\t" + y[3] + "\n");
+			
+			// adding the result list to output.txt
+			for (int i = 0; i < result.size(); i++) {
+				if(!set.contains(result.get(i))) {
+					set.add(result.get(i));
 					trainData[0] = result.get(i).get(0).toString().replaceAll("[\\[\\]\\,]", "");
 					trainData[1] = result.get(i).get(1).toString().replaceAll("[\\[\\]\\,]", "");
 					trainData[2] = result.get(i).get(2).toString().replaceAll("[\\[\\]\\,]", "");
 					trainData[3] = result.get(i).get(3).toString().replaceAll("[\\[\\]\\,]", "");
-					//bw.write(Arrays.toString(trainData) + "\n");
-					
-					bw.write(trainData[0] + "\t\t");
-					bw.write(trainData[1] + "\t\t");
-					bw.write(trainData[2] + "\t\t");
-					bw.write(trainData[3] + "\n");
+
+					fw.write(trainData[0] + "\t\t");
+					fw.write(trainData[1] + "\t\t");
+					fw.write(trainData[2] + "\t\t");
+					fw.write(trainData[3] + "\n");
 				}
-				
-				bw.close();
-	 		 
-			} 
-			
-			catch (IOException e) {
-				e.printStackTrace();
 			}
+			
+			fw.close();
+ 		} 
+					
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		catch (Exception e) {
+			e.getMessage();
+		}
 	}
 	
 	/*------------------------------------------------------*/
 
+	public static void letsCRUD() throws Exception, IOException {
+		try{
+			String options [] = {"DELETE", "UPDATE", "READ", "CREATE"};
+			JFrame frame = new JFrame("Frame");
+			int response = JOptionPane.showOptionDialog(null, "Select one of the following to modify data", "Train Entry Application",
+			        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+			        null, options, options[options.length-1]);
+			
+			switch(response) {
+				case 3: {
+					create();
+					break;
+				}
+					
+				case 2: {
+					read();
+					break;
+				}
+				
+				case 1: {
+					update();
+					break;
+				}
+				case 0: {
+					delete();
+					break;
+				}
+				
+				default: {
+					letsCRUD();								}
+			}
+		}
+		
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+/*****************************************************************/
+
+	//CRUD FUNCTIONS
 	
+	// create() - allows for creation of unique Entry, writes to trains.csv
+	// read() - reads from trains.csv
+	// update() - allows for updating of selected Entry in trains.csv
+	// delete() - deletes selected Entry from trains.csv
+	
+	//all functions also modify output.txt appropriately, 
+	// and keep data sorted by Run Number while also eliminating duplicate data
+	
+	/*------------------------------------------------------*/
+	
+	public static void create() throws IOException, Exception {
+		System.out.println("==========");
+		System.out.println("| CREATE |");
+		System.out.println("==========");
+		
+		//creating a JPanel for user input
+		
+		String[] options = {"OK"};
+		JPanel panel = new JPanel();
+		JLabel lbl = new JLabel("Enter a Train Entry: ");
+		JTextField txt = new JTextField(25);
+		panel.add(lbl);
+		panel.add(txt);
+		JOptionPane.showOptionDialog(null, panel, "CREATE ENTRY", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[0]);
+		String newEntry = txt.getText().toString(); 
+		ArrayList<String> item = CSVtoArrayList(newEntry); //holds the potential new Entry
+		String fileName = "trains.csv";
+		String line;
+		BufferedReader bufferedReader = null;
+		ArrayList<ArrayList<String>> bigList = new ArrayList<ArrayList<String>>(); //list of all the entries
+		
+		try {
+			bufferedReader = new BufferedReader(new FileReader(fileName));
+			
+			//read in the file
+			while ((line = bufferedReader.readLine()) != null) {
+				if(line.contains("TRAIN_LINE")) { //the first entry is the header so handled separately
+					y = line.split(",");
+				}
+
+				else { //convert each line of the CSV to an ArrayList<String>
+					ArrayList<String> list = CSVtoArrayList(line);
+						if(item.equals(list)){ //checks if its a duplicate
+							JOptionPane.showMessageDialog(new JFrame(), "DUPLICATE ENTRY! Please Try Again", "Dialog", JOptionPane.ERROR_MESSAGE);
+							create(); //if duplicate, create another one
+						}
+						else {
+							continue;
+						}
+				}
+			}
+		
+		}
+					
+		catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				finally {
+						try {
+							if (bufferedReader != null) 
+								bufferedReader.close();
+						} 
+						
+						catch (IOException exception) {
+							exception.printStackTrace();
+						}
+				}
+			
+		writeToCSV(item); //if success... write to CSV
+		readFromCSVFile();
+	}
+	/*------------------------------------------------------*/
+	
+	public static void read() throws IOException, Exception {
+		
+		System.out.println("==========");
+		System.out.println("|  READ  |");
+		System.out.println("==========");
+		StringBuilder sb = new StringBuilder();
+		
+		//Read File Line By Line from trains.csv
+		String fileName = "trains.csv";
+		String line;
+		JFrame frame= new JFrame();
+		BufferedReader bufferedReader = null;
+			try {
+				bufferedReader = new BufferedReader(new FileReader(fileName));
+				while ((line = bufferedReader.readLine()) != null)   {
+					String str [] = line.split(",");
+					
+					if(str.length > 0 && !str[0].equals("TRAIN_LINE")) {
+						sb.append(str[0] + "                 " + str[1] + "               " + str[2] + "           " + str[3]);
+						sb.append(System.getProperty("line.separator"));
+					}
+					if(str[0].equals("TRAIN_LINE")) {
+						sb.append(str[0] + " | " + str[1] + " | " + str[2] + " | " + str[3]);
+						sb.append(System.getProperty("line.separator"));
+						sb.append(System.getProperty("line.separator"));
+					}
+				}
+				JOptionPane.showMessageDialog(frame, sb.toString());
+				bufferedReader.close();
+
+			} 
+
+			catch (IOException e) {
+				e.getMessage();
+			}
+			
+			letsCRUD();
+	}
+	
+	/*------------------------------------------------------*/
+	
+	public static void update() throws IOException, Exception {
+		System.out.println("==========");
+		System.out.println("| UPDATE |");
+		System.out.println("==========");
+		JFrame frame= new JFrame();
+		
+		String selection = comboBox(); //grabs selection from combo box
+	   String updated = JOptionPane.showInputDialog(frame, "Edit Entry", selection); //allows you to update
+		ArrayList<String> edit = CSVtoArrayList(updated); 
+		deleteFromCSV(selection); //deletes old selection from trains.csv
+		writeToCSV(edit); // and writes in the updated selection
+	}
+	
+	/*------------------------------------------------------*/
+	
+	public static void delete() throws IOException, Exception {
+		System.out.println("==========");
+		System.out.println("| DELETE |");
+		System.out.println("==========");
+		JFrame frame = new JFrame();
+		String selection = comboBox(); //grabs selection from combo box
+		deleteFromCSV(selection); // deletes selection from csv file
+		JOptionPane.showMessageDialog(null, selection + " deleted!"); //outputs confirmation
+	}
+
+/*****************************************************************/
+
+
+
+
+//###############################################################//
+
+	// ### HELPER FUNCTIONS ###
+	
+	// comboBox() - reads all items from CSV file for selection, returns selection
+	// deleteFromCSV() - takes selection from comboBox and deletes it from CSV file
+	// writeToCSV() - writes entry to CSV file trains.csv
+	// CSVtoArrayList() - takes each entry from CSV and converts it to ArrayList<String>
+	// sortByRunNumber() - takes each entry (format: ArrayList<String>) and sorts based on Run Number
+	// indexOf() - finds index of the smallest (dictionary order) Run Number Value
+	// removeDuplicates() - removes duplicates from CSV file, and writes to output.txt
+	// stringify() - toString() representation of an Entry
+	// createEntries() - creation of Entry Objects based on a list of Entries from CSVToArrayList()
+	
+	/*------------------------------------------------------*/
+	
+	public static String comboBox() throws IOException, Exception {
+		ArrayList<String> updateList = new ArrayList<String>();
+		String fileName = "trains.csv";
+		String line;
+		JFrame frame= new JFrame();
+		BufferedReader bufferedReader = null;
+		String selection = null;
+			try {
+				bufferedReader = new BufferedReader(new FileReader(fileName));
+				while ((line = bufferedReader.readLine()) != null)   {				
+					if(!line.contains("TRAIN_LINE")) {
+						updateList.add(line);
+					}
+				}
+				 String [] options = new String[updateList.size()];
+					options = updateList.toArray(options);
+					
+				 //JFrame frame = new JFrame("Input Dialog Example 3");
+			    	selection = (String) JOptionPane.showInputDialog(frame, 
+			        "Select Entry To Update",
+			        "UPDATE",
+			        JOptionPane.QUESTION_MESSAGE, 
+			        null, 
+			        options, 
+			        options[0]);
+			
+				bufferedReader.close();
+			}
+				
+			catch(IOException e) {
+				e.getMessage();
+			}
+			
+			return selection;		
+	}
+		
+	/*------------------------------------------------------*/
+	
+	// the only way to delete from the file a specific value is to create a new file ... 
+	// ... and write all contents except input string  
+	public static void deleteFromCSV(String deleteThis) throws IOException, Exception {
+		File inputFile = new File("trains.csv");
+		File tempFile = new File("temp.csv");
+
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+		String lineToRemove = deleteThis;
+		String currentLine;
+
+		while((currentLine = reader.readLine()) != null) {
+		    String trimmedLine = currentLine.trim();
+		    if(trimmedLine.equals(lineToRemove)) {
+				continue; // if line == string to delete, skip over it
+			 }
+		    writer.write(currentLine + System.getProperty("line.separator")); //only write lines != to input string
+		}
+		writer.close(); 
+		reader.close(); 
+		
+		boolean successful = tempFile.renameTo(inputFile); //rename back to trains.csv so we don't create extra files
+	}
+	
+	/*------------------------------------------------------*/
+
+	public static void writeToCSV(ArrayList<String> entry) throws IOException, Exception {
+		try {
+			File file = new File("trains.csv");
+			 
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getName(), true);
+			BufferedWriter bw = new BufferedWriter(fw); // write entry which is an ArrayList<String> to file
+				
+			bw.write(entry.get(0) + ", "); 
+			bw.write(entry.get(1) + ", ");
+			bw.write(entry.get(2) + ", ");
+			bw.write(entry.get(3) + "\n");
+			
+			bw.close();
+ 		} 
+					
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		readFromCSVFile();
+		letsCRUD();
+
+	}
+	
+	/*------------------------------------------------------*/
+
 	public static ArrayList<String> CSVtoArrayList(String csv) {
 		ArrayList<String> result = new ArrayList<String>();
 		//transforming each line of the csv file to an ArrayList<String>
@@ -197,17 +480,17 @@ public class Entry {
 				}
 			}
 		}
-		
 		return result;
 	}
 	
 	/*------------------------------------------------------*/
 	
-	public static void createEntries(ArrayList<ArrayList<String>> bList){
+// code doesn't work, my intention was to make an ArrayList of Entry objects...
+// ... but the objects were getting overwritten
 		
-		//This Code didn't work, my intention was to make an ArrayList of Entry objects but the objects were getting overwritten
+	public static void createEntries(ArrayList<ArrayList<String>> bList) {
 
-		ArrayList<Entry> outList = new ArrayList<Entry>(); 
+		/* ArrayList<Entry> outList = new ArrayList<Entry>(); 
 		for (int x = 0; x < bList.size(); x++) {
 			ArrayList<String> list = new ArrayList<String>(); 
 			list = bList.get(x);
@@ -222,12 +505,13 @@ public class Entry {
 		}
 		
 		
-		/*for (int i = 0; i < outList.size(); i++) {
+		for (int i = 0; i < outList.size(); i++) {
 			
 			//System.out.println(outList.get(i).stringify());
 		
-		} */ 		
+		}
 		//System.out.println("OutList Size is: " + outList.size());
+		*/
 	}
 	
 	/*------------------------------------------------------*/
@@ -241,42 +525,36 @@ public class Entry {
 				minNumber = bList.get(i).get(2); //get the minimum number
 			}
 		}
+		
 		//find index of minimum Run Number in bList through helper function
 		index = indexOf(bList, minNumber);
-		//System.out.println("minNumber is: " + minNumber +", located at index: " + index);
 		
 		result.add(bList.get(index)); //add it to the result list...
 		bList.remove(bList.get(index)); //remove it from bList
 	}
 	
 	/*------------------------------------------------------*/
-	//helper function to get index of a particular String in the bigList
+	
+	//get index of a particular String in the bigList
 	public static int indexOf(ArrayList<ArrayList<String>> aL, String x) {
 		int index = 0;
 		for (int i = 0; i < aL.size(); i++) {
-			if(aL.get(i).get(2).equals(x)){
+			if(aL.get(i).get(2).equals(x)){ 
 				index = i;
-				return index;
+				return index; // the index needed by sortByRunNumber()
 			}
 		}
 		return -1;
 	}
+
+	/*------------------------------------------------------*/
 	
-	/*------------------------------------------------------*/
-	//prints the resulting train entry to console
-	public static void displayResult(ArrayList<ArrayList<String>> aL){
-		for(int i = 1; i < aL.size(); i++) {
-			System.out.println("[Entry " + i + "]: " + result.get(i));
-		}
-	}
-	 
-	/*------------------------------------------------------*/
 	//removes duplicate entries based on comparing the i-th and i+1-th index
 	//since the arrayList is now sorted it is easier to remove duplicates
 	public static void removeDuplicates(ArrayList<ArrayList<String>> aL) {
 		for (int i = 0; i < aL.size()-1; i++) {
-			if(aL.get(i).containsAll(aL.get(i+1))){
-				aL.remove(aL.get(i+1));
+			if(aL.get(i).containsAll(aL.get(i+1))) { //if two consecutive lists are indentical ...
+				aL.remove(aL.get(i+1)); //... remove the second list
 			}
 		}
 	}
@@ -290,5 +568,7 @@ public class Entry {
 	}
 	
 	/*------------------------------------------------------*/
+
+	//###############################################################//
 
 }
--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ __________________________________
 **				**	
 ** SAAD MAHMOOD			**    		
 ** WELLSPRING CODE SUBMISSION	**
-** MAY 21, 2015			**
+** MAY 24, 2015			**
 **				**
 __________________________________
 __________________________________
@@ -13,17 +13,23 @@ __________________________________
 ============================
 > ***IMPORTANT***
     - Keep all files in project folder
+    - CRUD functionality is merely a REPRESENTATION of a database, since I did not have access to a DB connection or server.
+
 > EXECUTION
-	- Run Entry.java, which will create output.txt
-	- open WellSpring.html in the browser
-	- choose 'output.txt' as the file
+	1. Run Entry.java, which will create output.txt
+	2. Execute CRUD Functionality
+		- NOTE: any input should be kept in CSV format,
+		- Example: “El, Blue Line, A000, JDoe” is accepted format.
+	3. perform any CRUD functionality before step 3. 
+	4. open WellSpring.html in the browser
+	5. choose 'output.txt' as the file
 	
 ============================
   WHAT THE PACKAGE CONTAINS
 ============================
-> Entry.java			=> .java file which reads the 'trains.csv'file and executes program to create the output file
+> Entry.java			=> .java file which reads the 'trains.csv' file and executes program to create the output file
 > output.txt			=>  a text file which holds the result of the operation to be processed on the webpage
 > WellSpring.html		=>  basic webpage which allows file selection
 > WellSpring.js			=> .js file which parses and loads the contents of output.txt into browser
-> trains.csv			=>  original input file which is read by Entry.java
+> trains.csv			=>  original input file which is read by Entry.java, modified through CRUD functionality
 > style.css			=>  .css file for UI purposes
